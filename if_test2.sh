6b31c75f959495740452f2cfce638dabdaa8b894
hntanda <zntanda@gmail.com>
2026-03-27 10:51:08 -0600

    created file if_test2.sh for nested if statements

---
+++ if_test2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+count=99
+
+if [ $count -eq 100 ]
+then 
+    echo "Count is greater than 100"
+else 
+    if [ $count -gt 100 ]
+    then
+        echo "Count is greater than 100"
+    else
+        echo "Count is less than 100"
+    fi
+fi 
+    
